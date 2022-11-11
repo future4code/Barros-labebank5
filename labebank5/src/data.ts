405569c88c58b3086170b4025a6541dfbd0865ad
Jessica Oliveira de Lima <jessicalimabitt@gmail.com>
2022-11-11 19:18:51 -0300

    atualizacao data

--- labebank5/src/data.ts
+++ labebank5/src/data.ts
@@ -7,7 +7,11 @@ export let client:Conta[] = [
         cpf: "000.000.000-00",
         dataNasc: "2001-08-05",
         saldo: 25000,
-        extratos:[ 20, 500, 1000, 90 ],
+        extratos:[ 
+            {valor: 200, data: "2022-08-14", descricao: "Compra1"}, 
+            {valor: 500, data: "2022-11-01", descricao: "Compra2"}, 
+            {valor: 300, data: "2022-11-05", descricao: "Compra3"}, 
+        ],
     },
     { 
         id: 2,
@@ -15,7 +19,11 @@ export let client:Conta[] = [
         cpf: "111.111.111-11",
         dataNasc: "1980-04-12",
         saldo: 2000,
-        extratos:[ 20, 5000, 90 ],
+        extratos:[
+            {valor: 2000, data: "2022-04-20", descricao: "Compra1"}, 
+            {valor: 100, data: "2022-02-05", descricao: "Compra2"}, 
+            {valor: 50, data: "2022-06-09", descricao: "Compra3"}, 
+        ],
     },
     { 
         id: 3,
@@ -23,7 +31,11 @@ export let client:Conta[] = [
         cpf: "333.333.333-33",
         dataNasc: "1995-05-25",
         saldo: 4500,
-        extratos:[ 20, 500, 50, 909 ],
+        extratos:[ 
+            {valor: 800, data: "2022-10-14", descricao: "Compra1"}, 
+            {valor: 60, data: "2022-10-30", descricao: "Compra2"}, 
+            {valor: 6000, data: "2022-11-08", descricao: "Compra3"}, 
+         ],
     }
 
 ]
